2c2ade3be5859c4a7f06309b93c9b37f049210c9
yoru <fikri36987@gmail.com>
2024-12-31 13:45:28 +0700

    fix : redirect wrong role

--- PBO_invent/src/java/servlets/register.java
+++ PBO_invent/src/java/servlets/register.java
@@ -6,6 +6,7 @@ package servlets;
 
 import classes.JDBC;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,21 +30,38 @@ public class register extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-            JDBC db = new JDBC();
-            if (db.isCon){
-                String username = request.getParameter("username");
-                String password = request.getParameter("password");
-                String cfmPass = request.getParameter("confirmPassword");
-                String role = "User";
-                if (password.equals(cfmPass)){
-                    db.runQuery("insert into user (username, password, role) VALUES ('" + username + "', '" + password + "','" + role + "')");
+        throws ServletException, IOException {
+    JDBC db = new JDBC();
+    if (db.isCon) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String cfmPass = request.getParameter("confirmPassword");
+        String role = "User";
+
+        try {
+            // Periksa apakah username sudah ada
+            ResultSet rs = db.getData("SELECT * FROM user WHERE username = '" + username + "'");
+            if (rs.next()) {
+                // Jika username sudah ada, redirect ke halaman register dengan status error
+                response.sendRedirect("register.jsp?status=usernameExists");
+            } else {
+                // Validasi password
+                if (password.equals(cfmPass)) {
+                    // Tambahkan user ke database
+                    db.runQuery("INSERT INTO user (username, password, role) VALUES ('" + username + "', '" + password + "', '" + role + "')");
                     response.sendRedirect("register.jsp?status=success");
                 } else {
-                    response.sendRedirect("register.jsp?status=error");
+                    // Password dan konfirmasi tidak cocok
+                    response.sendRedirect("register.jsp?status=passwordMismatch");
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace(); // Log error ke server
+            response.sendRedirect("register.jsp?status=error");
+        }
     }
+}
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
